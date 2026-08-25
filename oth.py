283bfec7b9fadbf6695e66b91d960509c38ed726
Scara <scara@2400-1a00-4b4d.ip6.wlink.com.np>
2026-08-26 02:46:42 +0545

    Updated outside area to return to Mom's Room

--- oth.py
+++ oth.py
@@ -1,5 +1,14 @@
 import functions
 
 def going_out(player):
-    functions.sleeper(["You go outside banging the door loud asl",
-                       "Your mom screams at you as usual"])
+    import momroom  
+    
+    functions.sleeper([
+        "You go outside banging the door loud asl",
+        "Your mom screams at you as usual"
+    ])
+    print("\nThis is still under construction man, play it after few days I'll update it I swear.")
+    
+    input("\nPress Enter to return to Mom's Room...")
+    
+    momroom.mom_room_menu(player)
